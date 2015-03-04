a7833ae3124a586ddb61edc34a7712e3951d9052
ph4nt0m1m3 <electro7bug@gmail.com>
2015-03-04 23:49:53 +0800

    2nd

--- x1install/install_core.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-
-require __DIR__.'/../bootstrap/autoload.php';
-require __DIR__.'/../vendor/autoload.php';
-
-use \Illuminate\Config\LoaderInterface as Config;
-
-$app_info['app_name'] = Config::get('');
